5b1e2a4fcb4f4189ec1f3029d69ced1fece67c1b
DDP <1321510155@qq.com>
2017-06-09 22:17:06 +0800

    17-6-9

---
+++ VVVV/MVE4/my6-9/src/components/heda.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="hello">
+   
+    <h2>Ecosystem</h2>
+    <ul>
+      <li >w zhe xiang cao  ni ma </a></li>
+      <my-da></my-da>
+      
+    </ul>
+  </div>  
+</template>
+
+<script>
+
+import MyDa from './ds'
+export default {
+  components: {
+MyDa
+    },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
