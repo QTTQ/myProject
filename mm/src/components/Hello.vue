3e75aee5d835678a36f4fa0e29e4d2c6eae37be2
Avrin <1321510155@qq.com>
2017-06-11 21:02:32 +0800

    6-11 elemui

---
+++ mm/src/components/Hello.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <div class="header">
+      <head-nav></head-nav>
+    </div>
+    <div class="content">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            <content-left></content-left>
+  
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <div class="grid-content bg-purple">
+  
+      <content-right></content-right>
+          </div>
+        </el-col>
+      </el-row>
+  
+            <div class="footer"></div>
+    </div>
+  </div>
+</template>  
+  
+<script>  
+import HeadNav from './pages/HeaderNav'
+import ContentLeft from './pages/ct-lt/ContentLeft'
+import ContentRight from './pages/ct-rt/ContentRight'
+
+export default {
+  name: 'hello',
+  components: {
+    HeadNav,
+    ContentLeft,
+    ContentRight
+  }
+}
+</script>  
+
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->  
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style> 
---
+++ mm/src/components/pages/ct-rt/ContentRight.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="m-ct-rt">
+
+    <right-header></right-header>
+
+  </div>
+</template>  
+  
+<script>  
+import RightHeader from './RightHeader'
+
+ export default {
+  name: 'm-ct-rt',
+  components: {
+   
+    RightHeader
+  }
+}
+</script>  
+
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->  
+<style scoped>
+
+</style> 
